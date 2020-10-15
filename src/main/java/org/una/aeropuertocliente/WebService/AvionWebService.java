feba174fc3ff806a6e55014d0b6a1c9ad02ee788
Esteban Vargas Ureña <estebanvargasu@gmail.com>
2020-10-15 15:14:02 -0600

    WebService Avion, HoraMarcaje

---
+++ src/main/java/org/una/aeropuertocliente/WebService/AvionWebService.java
@@ -0,0 +1,158 @@
+package org.una.aeropuertocliente.WebService;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.una.aeropuertocliente.DTOs.AvionDTO;
+import org.una.aeropuertocliente.utility.JSONUtils;
+/**
+ *
+ * @author Esteban Vargas
+ */
+public class AvionWebService {
+    
+    private static final HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2).build();
+    private static final String serviceURL = "http://localhost:8099/aviones";
+    
+    public static void getAllAviones() throws InterruptedException, ExecutionException, JsonParseException, JsonMappingException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+        List<AvionDTO> aviones = JSONUtils.convertFromJsonToList(response.get().body(), new TypeReference<List<AvionDTO>>() {});
+        aviones.forEach(System.out::println);
+        response.join();
+    }
+
+    public static void getAvionById() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/{id}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Avion No Encontrado");
+
+        else
+        {
+            AvionDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), AvionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+    public static void getAvionByMatricula() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/matricula/{termino}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Avion No Encontrado");
+
+        else
+        {
+            AvionDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), AvionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+    public static void getAvionByTipoAvion() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/TipoAvion/{termino}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Avion No Encontrado");
+
+        else
+        {
+            AvionDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), AvionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+    public static void getAvionByFechaRegistro() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/FechaRegistro/{termino}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Avion No Encontrado");
+
+        else
+        {
+            AvionDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), AvionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+    public static void getAvionByEstado() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/Estado/{termino}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Avion No Encontrado");
+
+        else
+        {
+            AvionDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), AvionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+    public static void createAerolinea(String matricula, String tipoAvion, Long aerolineaId) throws InterruptedException, ExecutionException, JsonProcessingException
+    {
+        AvionDTO bean = new AvionDTO();
+        
+        bean.setMatricula(matricula);
+        bean.setTipoAvion(tipoAvion);
+        bean.setAerolineaId(aerolineaId);
+
+        String inputJson = JSONUtils.covertFromObjectToJson(bean);
+        HttpRequest request = HttpRequest.newBuilder(URI.create(serviceURL+"/"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(inputJson)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.get().body());
+
+    }
+
+    public static void updateAerolinea(AvionDTO bean) throws InterruptedException, ExecutionException, IOException
+    {
+        String inputJson=JSONUtils.covertFromObjectToJson(bean);
+        HttpRequest request = HttpRequest.newBuilder(URI.create(serviceURL+"/{id}"))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(inputJson)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+
+        if(response.get().statusCode() == 500)
+            System.out.println("No se pudo actualizar el Avion");
+
+        else {
+            bean = JSONUtils.covertFromJsonToObject(response.get().body(), AvionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+}
---
+++ src/main/java/org/una/aeropuertocliente/WebService/HoraMarcajeWebService.java
@@ -0,0 +1,129 @@
+package org.una.aeropuertocliente.WebService;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.Date;
+import org.una.aeropuertocliente.DTOs.HoraMarcajeDTO;
+import org.una.aeropuertocliente.utility.JSONUtils;
+/**
+ *
+ * @author Esteban Vargas
+ */
+public class HoraMarcajeWebService {
+    
+    private static final HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2).build();
+    private static final String serviceURL = "http://localhost:8099/horaMarcaje";
+    
+
+    public static void getHoraMarcajeById() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/{id}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Hora de Marcaje No Encontrada");
+
+        else
+        {
+            HoraMarcajeDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), HoraMarcajeDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+    public static void getHoraMarcajeByHoraEntrada() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/HoraEntrada/{termino}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Hora de Marcaje No Encontrada");
+
+        else
+        {
+            HoraMarcajeDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), HoraMarcajeDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+    public static void getHoraMarcajeByHoraSalida() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/HoraSalida/{termino}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Hora de Marcaje No Encontrada");
+
+        else
+        {
+            HoraMarcajeDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), HoraMarcajeDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+    public static void getHoraMarcajeByFechaRegistro() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/FechaRegistro/{termino}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Hora de Marcaje No Encontrada");
+
+        else
+        {
+            HoraMarcajeDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), HoraMarcajeDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+    public static void createAerolinea(Date horaEntrada, Date horaSalida, Long usuarioId) throws InterruptedException, ExecutionException, JsonProcessingException
+    {
+        HoraMarcajeDTO bean = new HoraMarcajeDTO();
+        
+        bean.setHoraEntrada(horaEntrada);
+        bean.setHoraSalida(horaSalida);
+        bean.setUsuarioId(usuarioId);
+
+        String inputJson = JSONUtils.covertFromObjectToJson(bean);
+        HttpRequest request = HttpRequest.newBuilder(URI.create(serviceURL+"/"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(inputJson)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.get().body());
+
+    }
+
+    public static void updateAerolinea(HoraMarcajeDTO bean) throws InterruptedException, ExecutionException, IOException
+    {
+        String inputJson=JSONUtils.covertFromObjectToJson(bean);
+        HttpRequest request = HttpRequest.newBuilder(URI.create(serviceURL+"/{id}"))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(inputJson)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+
+        if(response.get().statusCode() == 500)
+            System.out.println("No se pudo actualizar la Hora de Marcaje");
+
+        else {
+            bean = JSONUtils.covertFromJsonToObject(response.get().body(), HoraMarcajeDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+}
