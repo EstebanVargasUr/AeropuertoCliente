e43c0136c2d90887a798508c0b00f32780d9692e
Esteban Vargas Ureña <estebanvargasu@gmail.com>
2020-10-15 16:19:16 -0600

    WebService Transaccion, Vuelo

---
+++ src/main/java/org/una/aeropuertocliente/WebService/TransaccionWebService.java
@@ -0,0 +1,92 @@
+package org.una.aeropuertocliente.WebService;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.una.aeropuertocliente.DTOs.TransaccionDTO;
+import org.una.aeropuertocliente.utility.JSONUtils;
+/**
+ *
+ * @author Esteban Vargas
+ */
+public class TransaccionWebService {
+    
+    private static final HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2).build();
+    private static final String serviceURL = "http://localhost:8099/transacciones";
+
+    public static void getTransaccionById() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/{id}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Transaccion No Encontrada");
+
+        else
+        {
+            TransaccionDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), TransaccionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+     public static void getTransaccionByEstado() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/{estado}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Transaccion No Encontrada");
+
+        else
+        {
+            TransaccionDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), TransaccionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+     
+    public static void createTransaccion(String informacion, Long usuarioId) throws InterruptedException, ExecutionException, JsonProcessingException
+    {
+        TransaccionDTO bean = new TransaccionDTO();
+        
+        bean.setInformacion(informacion);
+        bean.setUsuarioId(usuarioId);
+
+        String inputJson = JSONUtils.covertFromObjectToJson(bean);
+        HttpRequest request = HttpRequest.newBuilder(URI.create(serviceURL+"/"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(inputJson)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.get().body());
+
+    }
+
+    public static void updateTransaccion(TransaccionDTO bean) throws InterruptedException, ExecutionException, IOException
+    {
+        String inputJson=JSONUtils.covertFromObjectToJson(bean);
+        HttpRequest request = HttpRequest.newBuilder(URI.create(serviceURL+"/{id}"))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(inputJson)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+
+        if(response.get().statusCode() == 500)
+            System.out.println("No se pudo actualizar la Transaccion");
+
+        else {
+            bean = JSONUtils.covertFromJsonToObject(response.get().body(), TransaccionDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+}
---
+++ src/main/java/org/una/aeropuertocliente/WebService/VueloWebService.java
@@ -0,0 +1,145 @@
+package org.una.aeropuertocliente.WebService;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.una.aeropuertocliente.DTOs.VueloDTO;
+import org.una.aeropuertocliente.utility.JSONUtils;
+/**
+ *
+ * @author Esteban Vargas
+ */
+public class VueloWebService {
+    
+    private static final HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2).build();
+    private static final String serviceURL = "http://localhost:8099/vuelos";
+
+    public static void getVueloById() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/{id}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Vuelo No Encontrado");
+
+        else
+        {
+            VueloDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), VueloDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+    public static void getVueloByAeropuerto() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/Aeropuerto/{term}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Vuelo No Encontrado");
+
+        else
+        {
+            VueloDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), VueloDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+    public static void getVueloByEstado() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/{estado}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Vuelo No Encontrado");
+
+        else
+        {
+            VueloDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), VueloDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+    public static void getVueloByFechaSalida() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/FechaSalida/{term}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Vuelo No Encontrado");
+
+        else
+        {
+            VueloDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), VueloDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+    
+    public static void getVueloByFechaLlegada() throws InterruptedException, ExecutionException, IOException
+    {
+        HttpRequest req = HttpRequest.newBuilder(URI.create(serviceURL+"/FechaLlegada/{term}")).GET().build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(req, BodyHandlers.ofString());
+        response.thenAccept(res -> System.out.println(res));
+
+        if(response.get().statusCode() == 500)
+            System.out.println("Vuelo No Encontrado");
+
+        else
+        {
+            VueloDTO bean = JSONUtils.covertFromJsonToObject(response.get().body(), VueloDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+    public static void createVuelo(Float duracion, String aeropuerto, Long distancia, Long avionId) throws InterruptedException, ExecutionException, JsonProcessingException
+    {
+        VueloDTO bean = new VueloDTO();
+        
+        bean.setDuracion(duracion);
+        bean.setAeropuerto(aeropuerto);
+        bean.setDistancia(distancia);
+        bean.setAvionId(avionId);
+
+        String inputJson = JSONUtils.covertFromObjectToJson(bean);
+        HttpRequest request = HttpRequest.newBuilder(URI.create(serviceURL+"/"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(inputJson)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.get().body());
+
+    }
+
+    public static void updateVuelo(VueloDTO bean) throws InterruptedException, ExecutionException, IOException
+    {
+        String inputJson=JSONUtils.covertFromObjectToJson(bean);
+        HttpRequest request = HttpRequest.newBuilder(URI.create(serviceURL+"/{id}"))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(inputJson)).build();
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,HttpResponse.BodyHandlers.ofString());
+
+        if(response.get().statusCode() == 500)
+            System.out.println("No se pudo actualizar el Vuelo");
+
+        else {
+            bean = JSONUtils.covertFromJsonToObject(response.get().body(), VueloDTO.class);
+            System.out.println(bean);
+        }
+        response.join();
+    }
+
+}
