e932758273d175cf74decdf877129d587e5b7690
Esteban Vargas Ureña <estebanvargasu@gmail.com>
2020-11-03 10:27:23 -0600

    Preparando ventana de servicios para su implementación en código

--- AeropuertoCliente/src/main/java/org/una/aeropuertocliente/controllersView/ServicioController.java
+++ AeropuertoCliente/src/main/java/org/una/aeropuertocliente/controllersView/ServicioController.java
@@ -1,12 +1,17 @@
 package org.una.aeropuertocliente.controllersView;
 
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import org.una.aeropuertocliente.utility.FlowController;
@@ -24,6 +29,42 @@ public class ServicioController extends Controller implements  Initializable {
     private JFXTextField txt_buscar;
     @FXML
     private JFXComboBox<?> cb_filtro;
+    @FXML
+    private TableView<?> tablaServicios;
+    @FXML
+    private TableColumn<?, ?> tbc_id;
+    @FXML
+    private TableColumn<?, ?> tbc_tipoServicio;
+    @FXML
+    private TableColumn<?, ?> tbc_duracion;
+    @FXML
+    private TableColumn<?, ?> tbc_factura;
+    @FXML
+    private TableColumn<?, ?> tbc_encargado;
+    @FXML
+    private TableColumn<?, ?> tbc_fechaRegistro;
+    @FXML
+    private TableColumn<?, ?> tbc_fechaModificacion;
+    @FXML
+    private TableColumn<?, ?> tbc_estado;
+    @FXML
+    private TableColumn<?, ?> tbc_estadoCobro;
+    @FXML
+    private JFXTextArea txt_observaciones;
+    @FXML
+    private Label lbl_id;
+    @FXML
+    private Label lbl_matricula;
+    @FXML
+    private Label lbl_estado;
+    @FXML
+    private Label lbl_FechaRegistro;
+    @FXML
+    private Label lbl_aerolinea;
+    @FXML
+    private Label lbl_monto;
+    @FXML
+    private Label lbl_fecha;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -45,9 +86,16 @@ public class ServicioController extends Controller implements  Initializable {
         FlowController.getInstance().goView("MenuGestion");
     }
 
-    @FXML
     private void nuevo(MouseEvent event) {
         FlowController.getInstance().goView("CreacionServicio");
+    }
+
+    @FXML
+    private void buscar(KeyEvent event) {
+    }
+
+    @FXML
+    private void nuevoServicio(MouseEvent event) {
     }
     
 }
